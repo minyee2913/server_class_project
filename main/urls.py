21907709c8fad5a45f4f56056a5dfbce4ff0fa1f
minyee2913 <leegm0759@gmail.com>
2024-12-08 19:49:29 +0900

    ..

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
+# urls.py
 from django.urls import path
-
 from . import views
 
-
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.problem_set_list, name='problem_set_list'),
+    path('create/', views.problem_set_create, name='problem_set_create'),
+    path('edit/<int:problem_set_id>/', views.problem_set_edit, name='problem_set_edit'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,10 +1,38 @@
-from django.shortcuts import render
-
+# views.py
+from django.shortcuts import render, redirect
 from .models import ProblemSet, Problem
+from .forms import ProblemSetForm, ProblemForm
 
-# Create your views here.
-def index(request):
-    sets = ProblemSet.objects.all()
-    cells = Problem.objects.all()
+def problem_set_list(request):
+    problem_sets = ProblemSet.objects.all()
+    return render(request, 'problem_set_list.html', {'problem_sets': problem_sets})
 
-    return render(request,'main/index.html', {'sets': sets, 'cells': cells})
+def problem_set_create(request):
+    if request.method == 'POST':
+        form = ProblemSetForm(request.POST)
+        if form.is_valid():
+            problem_set = form.save()
+            return redirect('problem_set_edit', problem_set.id)
+    else:
+        form = ProblemSetForm()
+    return render(request, 'problem_set_create.html', {'form': form})
+
+def problem_set_edit(request, problem_set_id):
+    problem_set = ProblemSet.objects.get(id=problem_set_id)
+    problems = Problem.objects.filter(problem_set=problem_set)
+    if request.method == 'POST':
+        problem_form = ProblemForm(request.POST)
+        if problem_form.is_valid():
+            problem = problem_form.save(commit=False)
+            problem.problem_set = problem_set
+            problem.save()
+            problem_set.update_total_score()
+            return redirect('problem_set_edit', problem_set.id)
+    else:
+        problem_form = ProblemForm()
+
+    return render(request, 'problem_set_edit.html', {
+        'problem_set': problem_set,
+        'problem_form': problem_form,
+        'problems': problems,
+    })
