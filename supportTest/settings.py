5bb7462282184622d13416cb3cc7282ba2b81328
minyee2913 <leegm0759@gmail.com>
2024-12-09 00:14:28 +0900

    w

--- supportTest/settings.py
+++ supportTest/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -49,6 +50,8 @@ INSTALLED_APPS = [
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / "main/static"]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
